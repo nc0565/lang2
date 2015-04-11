a390b19fd23823648289720447b7c8bb1141d112
nc0565 <nc0565@my.bristol.ac.uk>
2015-04-11 20:43:26 +0100

    Added a project file

--- cwk2p1.hs
+++ cwk2p1.hs
@@ -9,9 +9,24 @@ type Z = Integer
 type T = Bool
 type State = Var -> Z
 
-data Aexp = N Integer | V Var | Add Aexp Aexp | Mult Aexp Aexp | Sub Aexp Aexp deriving (Show)
-data Bexp = TRUE | FALSE | Eq Aexp Aexp | Le Aexp Aexp | Neg Bexp | And Bexp Bexp deriving (Show)
-data Stm  = Ass Var Aexp | Skip | Comp Stm Stm | If Bexp Stm Stm | While Bexp Stm deriving (Show)
+data Aexp = N Integer
+            | V Var
+            | Add Aexp Aexp
+            | Mult Aexp Aexp
+            | Sub Aexp Aexp deriving (Show)
+            
+data Bexp = TRUE
+            | FALSE
+            | Eq Aexp Aexp
+            | Le Aexp Aexp
+            | Neg Bexp
+            | And Bexp Bexp deriving (Show)
+            
+data Stm  = Ass Var Aexp
+            | Skip
+            | Comp Stm Stm
+            | If Bexp Stm Stm
+            | While Bexp Stm deriving (Show)
 
 -- Semantic Functions (To Do)
 
